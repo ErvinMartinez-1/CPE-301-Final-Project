2e6b14136f9e8c41f51aaa0c220b66d1f733a9ce
VirtualGinger <125091643+VirtualGinger@users.noreply.github.com>
2025-12-10 19:57:51 -0800

    Added of ISR and interrupt logic, as well as the registers for interrupts
    I added the interupt logic for the buttons and LEDs as outputs

--- CPE301_Group25/CPE301_Group25.ino
+++ CPE301_Group25/CPE301_Group25.ino
@@ -29,12 +29,18 @@ volatile unsigned char *UCSR_0C = (unsigned char *) 0x00C2;
 volatile unsigned char *UBRR_0 = (unsigned char *) 0x00C4;
 volatile unsigned char *UDR_0 = (unsigned char *) 0x00C6;
 
+// Registers for ISR interrupts
+volatile unsigned char *myPCICR = (unsigned char *) 0x68;
+volatile unsigned char *myPCIFR = (unsigned char *) 0x3B;
+volatile unsigned char *myPCMSK0 = (unsigned char *) 0x6B;
+
 
 typedef enum STATE {DISABLED, IDLE, ERROR, RUNNING} STATE;
 STATE dev_state = DISABLED;
 
 
 void setup() {
+  IO_INIT();
   ADC_INIT();
   UART0_INIT(9600);
 }
@@ -51,6 +57,41 @@ void loop(){
         break;
   }
 }
+
+ISR(PCINT0_vect){
+  if (*PIN_B & RES_BTN){
+    if (dev_state == ERROR){
+      dev_state = IDLE;
+    }
+  } else if (*PIN_B & ST_BTN){
+    if (dev_state == RUNNING || dev_state == IDLE || dev_state == ERROR){
+      dev_state = DISABLED;
+    } else if (dev_state == DISABLED){
+      dev_state = IDLE;
+    }
+  }
+}
+
+void IO_INIT(){
+  *DDR_C  |= GLED_MASK;
+  *DDR_C  |= YLED_MASK;
+  *DDR_C  |= RLED_MASK;
+  *DDR_C  |= BLED_MASK;
+  *DDR_B  |= FAN_MASK;
+
+  *PORT_B |= ST_BTN;
+  *DDR_B  &= ~(ST_BTN);
+
+  *PORT_B |= RES_BTN;
+  *DDR_B  &= ~(RES_BTN);
+
+  *PORT_B |= CTRL_BTN;
+  *DDR_B  &= ~(CTRL_BTN);
+
+  *myPCICR |= 0x01;
+  *myPCMSK0 |= PCI_MASK;
+}
+
 void ADC_INIT(){
   *my_ADCSRA |=  0b10000000; 
   *my_ADCSRA &=  0b11011111; 
@@ -63,6 +104,7 @@ void ADC_INIT(){
   *my_ADMUX  &=  0b11011111; 
   *my_ADMUX  &=  0b11100000;
 }
+
 void UART0_INIT(unsigned long baud){
   unsigned long FCPU = 16000000;
   unsigned int tbaud = (FCPU / 16 / baud - 1);
